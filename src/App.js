bfe4a6067cc418cb7d163483635a91a71397f86c
Dina Lewkowicz <dina4488@walla.co.il>
2021-04-25 13:39:29 +0300

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
 import { HashRouter, Route, Switch } from 'react-router-dom';
+import NavBar from './component/NavBar/NavBar';
 import ActorsPage from './pages/ActorsPage/ActorsPage';
 import HomePage from './pages/HomePage/HomePage';
 import MoviePage from './pages/MoviePage/MoviePage';
@@ -9,14 +9,17 @@ import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
 function App() {
   return (
-    <HashRouter>
-        <Switch>
-            <Route exact path="/"><HomePage/></Route>
-            <Route exact path="/actors"><ActorsPage/> </Route>
-            <Route exact path="/movies"><MoviePage/></Route>
-            <Route path="/"><NotFoundPage/></Route>
-        </Switch>
-    </HashRouter>   
+    <div>
+      <NavBar></NavBar>
+      <HashRouter>
+          <Switch>
+              <Route exact path="/"><HomePage/></Route>
+              <Route exact path="/actors"><ActorsPage/> </Route>
+              <Route exact path="/movies"><MoviePage/></Route>
+              <Route path="/"><NotFoundPage/></Route>
+          </Switch>
+      </HashRouter>   
+    </div>
   );
 }
 
--- src/component/NavBar/NavBar.js
+++ src/component/NavBar/NavBar.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import { Button, Navbar, NavDropdown, Form , FormControl} from 'react-bootstrap';
+import { Button, Navbar, NavDropdown, Form , FormControl, Nav} from 'react-bootstrap';
 
 function NavBar({filterText , setFilterText, sortBy ,setSortBy }) {
     return (       
-    <Navbar bg="dark" variant="dark">
-        {/* <Navbar.Brand href="#home"></Navbar.Brand>
-        <Form inline>
-            <FormControl type="text" className="mr-sm-2"
-                value={filterText} onChange={(e) => setFilterText(e.target.value)} 
-                placeholder="Filter by a first or Last name" className="filterInp" />
-                <Form.Label>Example select</Form.Label>
-                <FormControl as="select" value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-                    <option value="firstName">First Name</option>
-                    <option value="lastName">Last Name</option>
-                    <option value="age">age</option>
-                </FormControl>
-        </Form>                      */}
-    </Navbar>
+        <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">           
+            <Nav.Link href="#/actors">Actors</Nav.Link> 
+            <Nav.Link href="#/movies">Movies</Nav.Link>
+          </Nav>
+          </Navbar.Collapse>
+          </Navbar>  
         
     );
 }
