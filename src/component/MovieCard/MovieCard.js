c6cee14e8fdf380e7d06deaec4ca408ef4f18e4a
Dina Lewkowicz <dina4488@walla.co.il>
2021-04-26 09:21:06 +0300

    fix MovieCard display

--- src/component/MovieCard/MovieCard.js
+++ src/component/MovieCard/MovieCard.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import { Card, Col } from 'react-bootstrap';
+import { Card, Col, Container , Row } from 'react-bootstrap';
 import './MovieCard.css';
 
 function MovieCard({movie}) {
-    return (       
-        <Col xs={12} md={6} lg={3}>              
-                <Card className="class-movie">
-                <Card.Img variant="top" src={movie.poster} />                 
-                    <Card.Body className="body">
-                        <Card.Title className="fixName">
-                           {movie.movieName}                        
-                            </Card.Title>                            
-                        <Card.Text>
-                       <span>Movie length: {movie.length}</span>     
-                       <span>Director:  {movie.director}</span>
-                       <span> Main actors: {movie.mainStars}</span>
-                        </Card.Text>                        
-                    </Card.Body>
-                </Card>
-            </Col>
-        
+    return ( 
+             <div className="class-movie">
+                   <img  src={movie.poster}/>                
+                <div className="fixName">
+                    <h3>{movie.movieName}   </h3>
+                    <p>Movie length: {movie.length}</p>     
+                    <p>Director:  {movie.director}</p>
+                    <p> Main actors: {movie.mainStars}</p>
+                </div>
+            </div>    
     );
 }
 
